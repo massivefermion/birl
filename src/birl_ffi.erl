8f5d8bd05610993db234de4dce768cb657b17c6b
shayan javani <massivefermion@protonmail.com>
2024-04-01 10:12:59 +0330

    fixed bug, bumped deps

--- src/birl_ffi.erl
+++ src/birl_ffi.erl
@@ -34,12 +34,15 @@ now() -> os:system_time(microsecond).
 
 local_offset() ->
     Timestamp = erlang:timestamp(),
-    {{LY, LN, LD}, {LH, LM, _}} = calendar:now_to_local_time(Timestamp),
-    {{UY, UN, UD}, {UH, UM, _}} = calendar:now_to_universal_time(Timestamp),
+    {{_, LMo, LD}, {LH, LM, _}} = calendar:now_to_local_time(Timestamp),
+    {{_, UMo, UD}, {UH, UM, _}} = calendar:now_to_universal_time(Timestamp),
     if
-        LD == UD -> (LH - UH) * 60 + LM - UM;
-        LD > UD orelse LN > UN orelse LY > UY -> (23 - UH) * 60 + (60 - UM) + LH * 60 + LM; 
-        LD < UD -> -((23 - LH) * 60 + (60 - LM) + UH * 60 + UM)
+        LD == UD ->
+            (LH - UH) * 60 + LM - UM;
+        LD > UD andalso LMo == UMo ->
+            (23 - UH) * 60 + (60 - UM) + LH * 60 + LM;
+        true ->
+            -((23 - LH) * 60 + (60 - LM) + UH * 60 + UM)
     end.
 
 local_timezone() ->
